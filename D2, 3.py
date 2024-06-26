ed247e6f465e33c83fb29c575e2c01cdf8e91b53
seham-s <seham.afaneh@yahoo.com>
2024-06-26 11:08:34 +0300

    Add files via upload

--- D2, 3.py
+++ D2, 3.py
@@ -7,4 +7,11 @@ def factorial(n: int) -> int:
     if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Function to compute a list of factorials from a list of numbers
+def list_factorial(lst: List[int]) -> List[int]:
+    return [factorial(number) for number in lst]
